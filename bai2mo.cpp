63068a5eef6c778f475a31d4963301fcace74c25
ADMIN <ADMIN@LAB1-MAY03>
2024-02-19 16:35:23 +0700

    Add project files.

---
+++ bai2mo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int  a, b;
+	float x;
+	cout << "UNG DUNG GIAI PHONG TRINH BAC I\n";
+	cout << "nhap a:"; cin >> a;
+	cout << "nhap b:"; cin >> b;
+	if (a == 0)
+	{
+    if (b==0)
+	{
+		cout << "PT dung voi moi x ~ vo so nghiem\n";
+	}
+	else {
+		cout << "PT vo nghiem\n";
+		{
+
+	    else{
+		x = (float)-b / a;
+		cout << "ketquax=" << x << endl;
+	}
+	return 0;
+}
